eb132503948277e8ab31e8fa81feddba62dc69b4
Brian Berns <brianberns@gmail.com>
2021-02-04 16:42:31 -0500

    Improved check for infinite evaluation regress.

--- Ski.Test/UnitTest.fs
+++ Ski.Test/UnitTest.fs
@@ -7,7 +7,14 @@ open Ski
 type UnitTest () =
 
     [<TestMethod>]
-    member __.SKSK () =
+    member __.SKSK() =
         let sksk = Node (Node (Node (S, K), S), K)
         Assert.AreEqual("SKSK", $"{sksk}")
         Assert.AreEqual(K, Ski.eval sksk)
+
+    [<TestMethod; Timeout(1000)>]
+    member __.SIISII() =
+        let sii = Node (Node (S, I), I)
+        let siisii = Node (sii, sii)
+        Assert.AreEqual("SII(SII)", $"{siisii}")
+        Assert.AreEqual(siisii, Ski.eval siisii)
--- Ski/Ski.fs
+++ Ski/Ski.fs
@@ -16,23 +16,31 @@ type Ski =
             | Node (x, I) -> $"{x.String}I"
             | Node (x, K) -> $"{x.String}K"
             | Node (x, S) -> $"{x.String}S"
-            | Node (x, y) -> $"({x.String}{y.String})"
+            | Node (x, y) -> $"{x.String}({y.String})"
 
     override this.ToString() =
         this.String
 
 module Ski =
 
-    let rec eval = function
-        | I -> I
-        | K -> K
-        | S -> S
-        | Node (I, x) -> eval x
-        | Node (Node (K, x), _) -> eval x
-        | Node (Node (Node (S, x), y), z) ->
-            eval (Node (Node (x, z), Node (y, z)))
-        | Node (x, y) as node ->
-            let x' = eval x
-            let y' = eval y
-            if x' = x && y' = y then node
-            else eval (Node (x', y'))
+    let eval ski =
+
+        let rec loop seen ski =
+
+            if seen |> Set.contains ski then ski
+            else
+                let loop = loop (seen |> Set.add ski)
+                match ski with
+                    | I -> I
+                    | K -> K
+                    | S -> S
+                    | Node (I, x) -> loop x
+                    | Node (Node (K, x), _) -> loop x
+                    | Node (Node (Node (S, x), y), z) ->
+                        loop (Node (Node (x, z), Node (y, z)))
+                    | Node (x, y) ->
+                        let x' = loop x
+                        let y' = loop y
+                        loop (Node (x', y'))
+
+        loop Set.empty ski
